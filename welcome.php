1b56564d9108dc473a7ba8fbd4a5dd965bed7621
lobYno <163894923+lobYno@users.noreply.github.com>
2024-04-02 22:51:07 +0200

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,12 +1,12 @@
 <?php
-session_start(); //otvorenie session
+session_start(); // Open session
 
-//zistenie ci je session nastavene
-if(isset($_SESSION['username']) ) {
-    echo '<link rel=stylesheet href="welcome.css">';
+// Check if session is set
+if(isset($_SESSION['username'])) {
+    echo '<link rel="stylesheet" href="welcome.css">';
     echo '<div class="zaklad">';
     echo '<p>Welcome <span>'.$_SESSION['username'].'</span></p><br>';
-    echo '<p>Click here to <a href = "logout.php" tite = "Logout">logout.</p>';//odkaz na odhlasenie
+    echo '<p class="logout-link">Click here to <a href="logout.php" title="Logout">logout.</a></p>'; // Logout link
     echo '</div>';
 }
 ?>
